883586df1ef5ef101e2f96d573f32950a7cdb5b1
AaronJThompson <aaronjthompson98@gmail.com>
2019-11-04 15:37:32 +0000

    Change to directed edges

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -18,7 +18,6 @@ class Graph:
         """
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
-            self.vertices[v2].add(v1)
         
     def bft(self, starting_vertex):
         """
