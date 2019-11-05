ef516220c11f875eaa43a9b66f031423b9e4605b
AaronJThompson <aaronjthompson98@gmail.com>
2019-11-05 16:31:03 +0000

    Create dictionary of parents

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,9 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    parents = dict()
+    for children in ancestors:
+      child = children[1]
+      if child not in parents:
+        parents[child] = set()
+      parents[child].add(children[0])
+    
