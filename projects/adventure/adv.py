96388d45d8b816e772668ff0b3a5ea736759ec35
AaronJThompson <aaronjthompson98@gmail.com>
2019-11-07 16:14:29 +0000

    Import stack

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -21,8 +21,22 @@ world.printRooms()
 
 player = Player("Name", world.startingRoom)
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+        
 # Fill this out
 traversalPath = []
+def createTraversalPath():
 
 
 
